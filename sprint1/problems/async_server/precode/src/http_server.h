16cf4af940d248d258636b8feba4437ff2dceebb
Andrew <p0se1d0n@yandex.ru>
2026-03-25 20:46:57 +0300

    async server started

--- sprint1/problems/async_server/precode/src/http_server.h
+++ sprint1/problems/async_server/precode/src/http_server.h
@@ -8,12 +8,16 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 
+#include <iostream>
+
 namespace http_server {
 
 namespace net = boost::asio;
 using tcp = net::ip::tcp;
 namespace beast = boost::beast;
 namespace http = beast::http;
+namespace sys = boost::system;
+using namespace std::literals;
 
 class SessionBase {
     // Напишите недостающий код, используя информацию из урока
@@ -24,9 +28,76 @@ class Session : public SessionBase, public std::enable_shared_from_this<Session<
 	// Напишите недостающий код, используя информацию из урока
 };
 
+inline void ReportError(beast::error_code ec, std::string_view what) {
+    std::cerr << what << ": "sv << ec.message() << std::endl;
+}
+
 template <typename RequestHandler>
 class Listener : public std::enable_shared_from_this<Listener<RequestHandler>> {
-    // Напишите недостающий код, используя информацию из урока
+public:
+    template <typename Handler>
+    Listener(net::io_context& ioc, const tcp::endpoint& endpoint, Handler&& request_handler)
+        : ioc_(ioc)
+        // Обработчики асинхронных операций acceptor_ будут вызываться в своём strand
+        , acceptor_(net::make_strand(ioc))
+        , request_handler_(std::forward<Handler>(request_handler)) {
+        // Открываем acceptor, используя протокол (IPv4 или IPv6), указанный в endpoint
+        acceptor_.open(endpoint.protocol());
+
+        // После закрытия TCP-соединения сокет некоторое время может считаться занятым,
+        // чтобы компьютеры могли обменяться завершающими пакетами данных.
+        // Однако это может помешать повторно открыть сокет в полузакрытом состоянии.
+        // Флаг reuse_address разрешает открыть сокет, когда он "наполовину закрыт"
+        acceptor_.set_option(net::socket_base::reuse_address(true));
+        // Привязываем acceptor к адресу и порту endpoint
+        acceptor_.bind(endpoint);
+        // Переводим acceptor в состояние, в котором он способен принимать новые соединения
+        // Благодаря этому новые подключения будут помещаться в очередь ожидающих соединений
+        acceptor_.listen(net::socket_base::max_listen_connections);
+    }
+
+    void Run() {
+        DoAccept();
+    }
+private:
+    void DoAccept() {
+        acceptor_.async_accept(
+            // Передаём последовательный исполнитель, в котором будут вызываться обработчики
+            // асинхронных операций сокета
+            net::make_strand(ioc_),
+            // С помощью bind_front_handler создаём обработчик, привязанный к методу OnAccept
+            // текущего объекта.
+            // Так как Listener — шаблонный класс, нужно подсказать компилятору, что
+            // shared_from_this — метод класса, а не свободная функция.
+            // Для этого вызываем его, используя this
+            // Этот вызов bind_front_handler аналогичен
+            // namespace ph = std::placeholders;
+            // std::bind(&Listener::OnAccept, this->shared_from_this(), ph::_1, ph::_2)
+            beast::bind_front_handler(&Listener::OnAccept, this->shared_from_this()));
+    }
+
+    // Метод socket::async_accept создаст сокет и передаст его в OnAccept
+    void OnAccept(sys::error_code ec, tcp::socket socket) {
+        using namespace std::literals;
+
+        if (ec) {
+            return ReportError(ec, "accept"sv);
+        }
+
+        // Асинхронно обрабатываем сессию
+        AsyncRunSession(std::move(socket));
+
+        // Принимаем новое соединение
+        DoAccept();
+    }
+
+    void AsyncRunSession(tcp::socket&& socket) {
+        // ...
+    }
+
+    net::io_context& ioc_;
+    tcp::acceptor acceptor_;
+    RequestHandler request_handler_;
 };
 
 template <typename RequestHandler>
