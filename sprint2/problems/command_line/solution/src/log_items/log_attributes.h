425195cb12090dc6797b7f0481a6fa52cc74751c
Andrew <p0se1d0nov@yandex.ru>
2026-04-17 17:54:38 +0300

    Deleted solution

--- sprint2/problems/command_line/solution/src/log_items/log_attributes.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "boost_json.h"
-#include "boost_log.h"
-
-// Определение ключевого слова для дополнительных данных
-BOOST_LOG_ATTRIBUTE_KEYWORD(additional_data, "AdditionalData",
-                            boost::json::value)
--- sprint2/problems/command_line/solution/src/utils/path_utils.h
+++
@@ -1,7 +0,0 @@
-#include <cassert>
-#include <filesystem>
-
-namespace util {
-bool IsSubPath(std::filesystem::path path, std::filesystem::path base);
-std::string url_decode(const std::string_view value);
-} // namespace util
